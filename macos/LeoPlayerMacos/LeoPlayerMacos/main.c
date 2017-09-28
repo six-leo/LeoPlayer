5f0931834917be4546cf94e486ab5dd6a7ec2083
liufulj <lfljjun123@gmail.com>
2017-09-28 21:54:31 +0800

    update

---
+++ macos/LeoPlayerMacos/LeoPlayerMacos/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LeoPlayerMacos
+//
+//  Created by xiaoliu on 2017/9/28.
+//  Copyright © 2017年 xiaoliu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
